40df6288ff7673e98a2abb35a44ee7420758f959
Corum Gold <goldcorum@gmail.com>
2021-06-03 12:35:49 -0400

    add 'not' demonstration

--- logical-operators.js
+++ logical-operators.js
@@ -11,14 +11,26 @@
 // 10-65 $20
 // 65+ free
 
-const age = 70;
+// const age = 70;
 
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log('FREE')
-} else if (age >=5 && age > 10) {
-    console.log('$10')
-} else if (age >=10 && age < 65) {
-    console.log('$20')
-} else {
-    console.log('invalid age')
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log('FREE')
+// } else if (age >=5 && age > 10) {
+//     console.log('$10')
+// } else if (age >=10 && age < 65) {
+//     console.log('$20')
+// } else {
+//     console.log('invalid age')
+// }
+
+
+// const firstName = prompt("Enter your first name")
+// if(!firstName){
+//     prompt("Try again!")
+// }
+
+
+const age = 45
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log('YOU ARE NOT A BABY OR A SENIOR!')
 }
