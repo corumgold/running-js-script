641cb5146967d2a7f483cda1ff15839e8502af18
Corum Gold <goldcorum@gmail.com>
2021-06-03 12:29:03 -0400

    add 'or' demonstration

--- logical-operators.js
+++ logical-operators.js
@@ -1,7 +1,24 @@
-const password = prompt('Enter a password');
+// const password = prompt('Enter a password');
 
-if (password.length >= 6 && password.indexOf(' ') === -1){
-    console.log('valid password!')
+// if (password.length >= 6 && password.indexOf(' ') === -1){
+//     console.log('valid password!')
+// } else {
+//     console.log('invalid password')
+// }
+
+// 0-5 free
+// 5-10 $10
+// 10-65 $20
+// 65+ free
+
+const age = 70;
+
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log('FREE')
+} else if (age >=5 && age > 10) {
+    console.log('$10')
+} else if (age >=10 && age < 65) {
+    console.log('$20')
 } else {
-    console.log('invalid password')
+    console.log('invalid age')
 }
